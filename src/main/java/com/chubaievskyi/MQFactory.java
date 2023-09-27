b73ea1f3ac0c8fead6b80ff9112699e059170019
Pavlo Chubaievskyi <chubaievskyi@gmail.com>
2023-09-27 22:37:48 +0300

    Added some changes

--- src/main/java/com/chubaievskyi/MQFactory.java
+++ src/main/java/com/chubaievskyi/MQFactory.java
@@ -1,107 +1,143 @@
-package com.chubaievskyi;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
-
-import java.util.List;
-import java.util.Properties;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.chubaievskyi.Main.LOGGER;
-
-public class MQFactory {
-
-    private static final Properties PROPERTIES = new PropertiesLoader().loadProperties();
-    private static final InputReader INPUT_READER = new InputReader(PROPERTIES);
-    private static final String WIRE_LEVEL_ENDPOINT = INPUT_READER.getWireLevelEndpoint();
-    private static final String USER_NAME = INPUT_READER.getUsername();
-    private static final String PASSWORD = INPUT_READER.getPassword();
-//    private static final String QUEUE_NAME = INPUT_READER.getQueueName();
-//    private static final long STOP_TIME = INPUT_READER.getStopTime();
-//    private static final int NUMBER_OF_MESSAGES = INPUT_READER.getNumberOfMessages();
-    private static final int NUMBER_OF_PRODUCER = INPUT_READER.getNumberOfProducer();
-    private static final int NUMBER_OF_CONSUMER = INPUT_READER.getNumberOfConsumer();
-
-//    private final AtomicInteger sendMessageCounter = new AtomicInteger(0);
-//    private final AtomicInteger receiveMessageCounter = new AtomicInteger(0);
-    public void run() {
-
-        ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
-        PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
-        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_PRODUCER + NUMBER_OF_CONSUMER);
-
-        long startTimeProducer = System.currentTimeMillis();
-        for (int i = 0; i < NUMBER_OF_PRODUCER; i++) {
-            executorService.submit(new Producer(
-                    pooledConnectionFactory,
-//                    QUEUE_NAME,
-//                    NUMBER_OF_MESSAGES,
-//                    NUMBER_OF_PRODUCER,
-//                    NUMBER_OF_CONSUMER,
+//package com.chubaievskyi;
+//
+//import org.apache.activemq.ActiveMQConnectionFactory;
+//import org.apache.activemq.jms.pool.PooledConnectionFactory;
+//
+//import java.util.List;
+//import java.util.Properties;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//import static com.chubaievskyi.Main.LOGGER;
+//
+//public class MQFactory {
+//
+//    private static final Properties PROPERTIES = new PropertiesLoader().loadProperties();
+//    private static final InputReader INPUT_READER = new InputReader(PROPERTIES);
+//    private static final String WIRE_LEVEL_ENDPOINT = INPUT_READER.getWireLevelEndpoint();
+//    private static final String USER_NAME = INPUT_READER.getUsername();
+//    private static final String PASSWORD = INPUT_READER.getPassword();
+////    private static final String QUEUE_NAME = INPUT_READER.getQueueName();
+////    private static final long STOP_TIME = INPUT_READER.getStopTime();
+////    private static final int NUMBER_OF_MESSAGES = INPUT_READER.getNumberOfMessages();
+//    private static final int NUMBER_OF_PRODUCER = INPUT_READER.getNumberOfProducer();
+//    private static final int NUMBER_OF_CONSUMER = INPUT_READER.getNumberOfConsumer();
+//    private final AtomicInteger activeProducerCount = new AtomicInteger(NUMBER_OF_PRODUCER);
+//
+////    private final AtomicInteger sendMessageCounter = new AtomicInteger(0);
+////    private final AtomicInteger receiveMessageCounter = new AtomicInteger(0);
+//    public void run() {
+//
+//        ActiveMQConnectionFactory connectionFactory = createActiveMQConnectionFactory();
+//        PooledConnectionFactory pooledConnectionFactory = createPooledConnectionFactory(connectionFactory);
+////        ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OF_PRODUCER + NUMBER_OF_CONSUMER);
+//
+//        ExecutorService producerExecutor = Executors.newFixedThreadPool(NUMBER_OF_PRODUCER);
+//        ExecutorService consumerExecutor = Executors.newFixedThreadPool(NUMBER_OF_CONSUMER);
+//
+//
+//        AtomicInteger totalSentMessages = new AtomicInteger(0);
+//        AtomicInteger totalReceivedMessages = new AtomicInteger(0);
+//
+//
+//        long startTimeProducer = System.currentTimeMillis();
+//        for (int i = 0; i < NUMBER_OF_PRODUCER; i++) {
+//            Producer producer = new Producer(
+//                    pooledConnectionFactory,
+//                    INPUT_READER,
 //                    startTimeProducer,
-//                    STOP_TIME,
-                    INPUT_READER,
-                    startTimeProducer
-                    ));
-        }
-
-        long startTimeConsumer = System.currentTimeMillis();
-        for (int i = 0; i < NUMBER_OF_CONSUMER; i++) {
-            executorService.submit(new Consumer(
-                    connectionFactory,
-//                    QUEUE_NAME,
-//                    receiveMessageCounter
-                    INPUT_READER
-            ));
-        }
-        long endTimeProducer = System.currentTimeMillis();
-        long endTimeConsumer = System.currentTimeMillis();
-        executorService.shutdown();
-
-        try {
-            if (!executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
-                LOGGER.error("Not all threads have terminated.");
-            }
-        } catch (InterruptedException e) {
-            LOGGER.debug("Executor service interrupted.", e);
-            Thread.currentThread().interrupt();
-        }
-
-        pooledConnectionFactory.stop();
-
-        double producerTime = (double) (endTimeProducer - startTimeProducer) / 1000;
-        LOGGER.info("Producer time (sec) - {}", producerTime);
-        LOGGER.info("Number of sent messages - {}", sendMessageCounter);
-        double consumerTime = (double) (endTimeConsumer - startTimeConsumer) / 1000;
-        LOGGER.info("Consumer time (sec) - {}", consumerTime);
-        LOGGER.info("Number of received messages - {}", receiveMessageCounter);
-
-        double averageEndingSpeed = sendMessageCounter.get() / producerTime;
-        double averageReceivingSpeed = receiveMessageCounter.get() / consumerTime;
-        String formattedAverageEndingSpeed = String.format("%.2f", averageEndingSpeed);
-        String formattedAverageReceivingSpeed = String.format("%.2f", averageReceivingSpeed);
-        LOGGER.info("Average speed of sending messages (messages per second) - {}", formattedAverageEndingSpeed);
-        LOGGER.info("Average speed of receiving messages (messages per second) - {}", formattedAverageReceivingSpeed);
-    }
-
-    private ActiveMQConnectionFactory createActiveMQConnectionFactory() {
-        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(WIRE_LEVEL_ENDPOINT);
-        connectionFactory.setTrustedPackages(List.of("com.chubaievskyi"));
-        LOGGER.info("Created a connection factory.");
-
-        connectionFactory.setUserName(USER_NAME);
-        connectionFactory.setPassword(PASSWORD);
-        return connectionFactory;
-    }
-
-    private PooledConnectionFactory createPooledConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
-        PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
-        pooledConnectionFactory.setConnectionFactory(connectionFactory);
-        pooledConnectionFactory.setMaxConnections(10);
-        LOGGER.info("Created a pooled connection factory.");
-        return pooledConnectionFactory;
-    }
-}
+//                    totalSentMessages,
+//                    activeProducerCount
+//                    );
+////            executorService.submit(producer);
+//            producerExecutor.submit(producer);
+//        }
+//
+//        long startTimeConsumer = System.currentTimeMillis();
+//        for (int i = 0; i < NUMBER_OF_CONSUMER; i++) {
+//            Consumer consumer = new Consumer(
+//                    connectionFactory,
+//                    INPUT_READER,
+//                    totalReceivedMessages
+//            );
+////            executorService.submit(consumer);
+//            consumerExecutor.submit(consumer);
+//        }
+//
+//        // Завершуємо роботу продюсерів
+//        producerExecutor.shutdown();
+//
+//        try {
+//            if (!producerExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+//                LOGGER.error("Not all producer threads have terminated.");
+//            }
+//        } catch (InterruptedException e) {
+//            LOGGER.debug("Producer executor service interrupted.", e);
+//            Thread.currentThread().interrupt();
+//        }
+//        long endTimeProducer = System.currentTimeMillis();
+//
+//// Завершуємо роботу консюмерів
+//        consumerExecutor.shutdown();
+//
+//        try {
+//            if (!consumerExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+//                LOGGER.error("Not all consumer threads have terminated.");
+//            }
+//        } catch (InterruptedException e) {
+//            LOGGER.debug("Consumer executor service interrupted.", e);
+//            Thread.currentThread().interrupt();
+//        }
+//        long endTimeConsumer = System.currentTimeMillis();
+//
+//
+////        executorService.shutdown();
+////
+////        try {
+////            if (!executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS)) {
+////                LOGGER.error("Not all threads have terminated.");
+////            }
+////        } catch (InterruptedException e) {
+////            LOGGER.debug("Executor service interrupted.", e);
+////            Thread.currentThread().interrupt();
+////        }
+////        long endTimeProducer = System.currentTimeMillis();
+////        long endTimeConsumer = System.currentTimeMillis();
+//
+//        pooledConnectionFactory.stop();
+//
+//        double producerTime = (double) (endTimeProducer - startTimeProducer) / 1000;
+//        LOGGER.info("Producer time (sec) - {}", producerTime);
+//        LOGGER.info("Number of sent messages - {}", totalSentMessages);
+//        double consumerTime = (double) (endTimeConsumer - startTimeConsumer) / 1000;
+//        LOGGER.info("Consumer time (sec) - {}", consumerTime);
+//        LOGGER.info("Number of received messages - {}", totalReceivedMessages);
+//
+//        double averageEndingSpeed = totalSentMessages.get() / producerTime;
+//        double averageReceivingSpeed = totalReceivedMessages.get() / consumerTime;
+//        String formattedAverageEndingSpeed = String.format("%.2f", averageEndingSpeed);
+//        String formattedAverageReceivingSpeed = String.format("%.2f", averageReceivingSpeed);
+//        LOGGER.info("Average speed of sending messages (messages per second) - {}", formattedAverageEndingSpeed);
+//        LOGGER.info("Average speed of receiving messages (messages per second) - {}", formattedAverageReceivingSpeed);
+//    }
+//
+//    private ActiveMQConnectionFactory createActiveMQConnectionFactory() {
+//        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(WIRE_LEVEL_ENDPOINT);
+//        connectionFactory.setTrustedPackages(List.of("com.chubaievskyi"));
+//        LOGGER.info("Created a connection factory.");
+//
+//        connectionFactory.setUserName(USER_NAME);
+//        connectionFactory.setPassword(PASSWORD);
+//        return connectionFactory;
+//    }
+//
+//    private PooledConnectionFactory createPooledConnectionFactory(ActiveMQConnectionFactory connectionFactory) {
+//        PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+//        pooledConnectionFactory.setConnectionFactory(connectionFactory);
+//        pooledConnectionFactory.setMaxConnections(100);
+//        LOGGER.info("Created a pooled connection factory.");
+//        return pooledConnectionFactory;
+//    }
+//}
--- src/main/java/com/chubaievskyi/Producer.java
+++ src/main/java/com/chubaievskyi/Producer.java
@@ -1,102 +1,92 @@
-package com.chubaievskyi;
-
-import org.apache.activemq.jms.pool.PooledConnectionFactory;
-import javax.jms.*;
-import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.chubaievskyi.Main.LOGGER;
-
-public class Producer implements Runnable {
-    private static final UserGenerator USER_GENERATOR = new UserGenerator();
-    private final AtomicInteger sendMessageCounter = new AtomicInteger(0);
-
-    private final PooledConnectionFactory pooledConnectionFactory;
-    private final String queueName;
-    private final int numberOfMessages;
-    private final int numberOfConsumer;
-    private final long stopTime;
-    private final AtomicInteger activeProducerCount;
-    private long startTimeProducer;
-//    private long endTimeProducer;
-
-    public Producer(PooledConnectionFactory pooledConnectionFactory, InputReader inputReader, long startTimeProducer) {
-        this.pooledConnectionFactory = pooledConnectionFactory;
-        this.queueName = inputReader.getQueueName();
-        this.numberOfMessages = inputReader.getNumberOfMessages();
-        this.numberOfConsumer = inputReader.getNumberOfConsumer();
-        this.stopTime = inputReader.getStopTime();
-        this.activeProducerCount = new AtomicInteger(inputReader.getNumberOfProducer());
-        this.startTimeProducer = startTimeProducer;
-    }
-
-    @Override
-    public void run() {
-//        startTimeProducer = System.currentTimeMillis();
-        try {
-            sendMessage();
-        } catch (JMSException | IOException e) {
-            LOGGER.debug("Error sending a message.", e);
-        }
-//        endTimeProducer = System.currentTimeMillis();
-//        double producerTime = (double) (endTimeProducer - startTimeProducer) / 1000;
-//        LOGGER.info("Producer time (sec) - {}", producerTime);
-//        LOGGER.info("Number of sent messages - {}", sendMessageCounter);
+//package com.chubaievskyi;
 //
-//        double averageEndingSpeed = sendMessageCounter.get() / producerTime;
-//        String formattedAverageEndingSpeed = String.format("%.2f", averageEndingSpeed);
-//        LOGGER.info("Average speed of sending messages (messages per second) - {}", formattedAverageEndingSpeed);
-    }
-
-    private void sendMessage() throws JMSException, IOException {
-        Connection producerConnection = pooledConnectionFactory.createConnection();
-        producerConnection.start();
-        LOGGER.info("Connection with the producer is established.");
-
-        Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        LOGGER.info("Created a session.");
-
-        Destination producerDestination = producerSession.createQueue(queueName);
-        LOGGER.info("Created a queue.");
-
-        MessageProducer producer = producerSession.createProducer(producerDestination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        LOGGER.info("Created a producer from the session to the queue.");
-
-        LOGGER.info("Start sending messages to the queue.");
-        long stopTimeProducer = startTimeProducer + (stopTime * 1000);
-        while (sendMessageCounter.get() < numberOfMessages || System.currentTimeMillis() >= stopTimeProducer) {
-            if (Thread.currentThread().isInterrupted()) {
-                LOGGER.debug("Producer is interrupted.");
-                break;
-            }
-
-            String text = USER_GENERATOR.generateRandomUser();
-            TextMessage producerMessage = producerSession.createTextMessage(text);
-            producer.send(producerMessage);
-            if (sendMessageCounter.incrementAndGet() % 2 == 0) {
-                LOGGER.info("Message sent: {}", text);
-//                LOGGER.info("{} message sent.", sendMessageCounter.get());
-            }
-
-//            if (sendMessageCounter.incrementAndGet() % 10000 == 0) {
+//import org.apache.activemq.jms.pool.PooledConnectionFactory;
+//import javax.jms.*;
+//import java.io.IOException;
+//import java.util.concurrent.atomic.AtomicInteger;
+//
+//import static com.chubaievskyi.Main.LOGGER;
+//
+//public class Producer implements Runnable {
+//    private static final UserGenerator USER_GENERATOR = new UserGenerator();
+//    private AtomicInteger sendMessageCounter = new AtomicInteger(0);
+//
+//    private final PooledConnectionFactory pooledConnectionFactory;
+//    private final String queueName;
+//    private final int numberOfMessages;
+//    private final int numberOfConsumer;
+//    private final long stopTime;
+//    private final AtomicInteger activeProducerCount;
+//    private long startTimeProducer;
+//    private long endTimeProducer;
+//
+//    public Producer(PooledConnectionFactory pooledConnectionFactory, InputReader inputReader, long startTimeProducer, AtomicInteger sendMessageCounter, AtomicInteger activeProducerCount) {
+//        this.pooledConnectionFactory = pooledConnectionFactory;
+//        this.queueName = inputReader.getQueueName();
+//        this.numberOfMessages = inputReader.getNumberOfMessages();
+//        this.numberOfConsumer = inputReader.getNumberOfConsumer();
+//        this.stopTime = inputReader.getStopTime();
+////        this.activeProducerCount = new AtomicInteger(inputReader.getNumberOfProducer());
+//        this.activeProducerCount = activeProducerCount;
+//        this.startTimeProducer = startTimeProducer;
+//        this.sendMessageCounter = sendMessageCounter;
+//    }
+//
+//    @Override
+//    public void run() {
+////        startTimeProducer = System.currentTimeMillis();
+//        try {
+//            sendMessage();
+//        } catch (JMSException | IOException e) {
+//            LOGGER.debug("Error sending a message.", e);
+//        }
+////        endTimeProducer = System.currentTimeMillis();
+//    }
+//
+//    private void sendMessage() throws JMSException, IOException {
+//        Connection producerConnection = pooledConnectionFactory.createConnection();
+//        producerConnection.start();
+//        LOGGER.info("Connection with the producer is established.");
+//
+//        Session producerSession = producerConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//        LOGGER.info("Created a session.");
+//
+//        Destination producerDestination = producerSession.createQueue(queueName);
+//        LOGGER.info("Created a queue.");
+//
+//        MessageProducer producer = producerSession.createProducer(producerDestination);
+//        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+//        LOGGER.info("Created a producer from the session to the queue.");
+//
+//        LOGGER.info("Start sending messages to the queue.");
+//        long stopTimeProducer = startTimeProducer + (stopTime * 1000);
+//        while (sendMessageCounter.get() < numberOfMessages || System.currentTimeMillis() >= stopTimeProducer) {
+//            if (Thread.currentThread().isInterrupted()) {
+//                LOGGER.debug("Producer is interrupted.");
+//                break;
+//            }
+//
+//            String text = USER_GENERATOR.generateRandomUser();
+//            TextMessage producerMessage = producerSession.createTextMessage(text);
+//            producer.send(producerMessage);
+//            if (sendMessageCounter.incrementAndGet() % 2 == 0) {
 //                LOGGER.info("Message sent: {}", text);
 ////                LOGGER.info("{} message sent.", sendMessageCounter.get());
 //            }
-        }
-
-        int remainingProducers = activeProducerCount.decrementAndGet();
-        if (remainingProducers == 0) {
-            for (int i = 0; i < numberOfConsumer; i++) {
-                TextMessage poisonPill = producerSession.createTextMessage("Poison Pill");
-                producer.send(poisonPill);
-            }
-            LOGGER.info("Poison Pill sent to signal the end of production.");
-        }
-
-        producer.close();
-        producerSession.close();
-        producerConnection.close();
-    }
-}
-
+//        }
+//
+//        int remainingProducers = activeProducerCount.decrementAndGet();
+//        if (remainingProducers == 0) {
+//            for (int i = 0; i < numberOfConsumer; i++) {
+//                TextMessage poisonPill = producerSession.createTextMessage("Poison Pill");
+//                producer.send(poisonPill);
+//            }
+//            LOGGER.info("Poison Pill sent to signal the end of production.");
+//        }
+//
+//        producer.close();
+//        producerSession.close();
+//        producerConnection.close();
+//    }
+//}
+//
